0cb98bd7c777cfaee655deb30c7b6976f23e62ee
NicGrotle <ngrotle@uccs.edu>
2022-04-20 20:30:40 +0000

    Iteration 5

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :set_project, only: %i[ show edit update destroy ]
+  
 
   # GET /projects or /projects.json
   def index
