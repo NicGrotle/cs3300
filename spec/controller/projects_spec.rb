fd011371ec18e4e2ba5b4d9c685bce694caf4f5b
NicGrotle <ngrotle@uccs.edu>
2022-04-23 22:07:13 +0000

    iteration5

--- spec/controller/projects_spec.rb
+++ spec/controller/projects_spec.rb
@@ -1,4 +1,5 @@
 require "rails_helper"
+
 RSpec.describe ProjectsController, type: :controller do
   context "GET #index" do
     it "returns a success response" do
@@ -7,12 +8,13 @@ RSpec.describe ProjectsController, type: :controller do
       expect(response).to be_success
     end
   end
+
   context "GET #show" do
     let!(:project) { Project.create(title: "Test title", description: "Test description") }
     it "returns a success response" do
       get :show, params: { id: project }
       expect(response).to be_success
+
     end
   end
 end
-
